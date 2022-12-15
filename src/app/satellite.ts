42d321d3f3a1343f88a9424f5655db760d173b9b
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2022-12-14 19:39:28 -0600

    made an update to isSpaceDebris function to pass autograder

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -17,8 +17,10 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
-	if(this.type === 'Space Debris')
+	if(this.type === 'Space Debris'){
 		return true;
+   } else{
+		return false;
    }
 
 }
